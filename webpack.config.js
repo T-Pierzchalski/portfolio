cb2c99a00285f5d27eadeb3b6ec6af238699b3c6
TrueDie3 <t.pierzchalski0@gmail.com>
2023-03-04 14:36:09 +0100

    ip

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+	entry: {
+		main: "./src/js/main.js",
+		bootstrap: "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+	},
+	output: {
+		filename: "index.js",
+		path: path.resolve(__dirname, "js"),
+	},
+};
