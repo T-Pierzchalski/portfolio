cc8b13a954f4acfc07924d0f43b53c20b7192118
TrueDie3 <t.pierzchalski0@gmail.com>
2023-03-04 15:02:39 +0100

    push

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require("path");
 
 module.exports = {
 	entry: {
-		main: "./src/js/main.js",
-		bootstrap: "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+		bootstrap: path.resolve(
+			__dirname,
+			"node_modules/bootstrap/dist/js/bootstrap.bundle.min.js/"
+		),
 	},
 	output: {
 		filename: "index.js",
-		path: path.resolve(__dirname, "src", "js"),
+		path: path.resolve(__dirname, "src/js"),
 	},
 };
